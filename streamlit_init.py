6571169bd66d2fdc26a3887ca08071cd07a90c26
thearhansheth <arhan.sheth@gmail.com>
2024-07-09 16:58:22 +0530

    Removed flask application and display data using streamlit instead

--- streamlit_init.py
+++ streamlit_init.py
@@ -30,12 +30,12 @@ emp_connection.execute('''INSERT INTO employee_data VALUES(
 print("Data Insertion Successul!")
 
 # Initializing Flask
-app = Flask(__name__)
-@app.route("/getData/<name>")
-def getData(name):
-        emp_cursor.execute("SELECT * FROM employee_data WHERE NAME = ?", (name,))
-        final_data = emp_cursor.fetchall()
-        return final_data
+# app = Flask(__name__)
+# @app.route("/getData/<name>")
+# def getData(name):
+#         emp_cursor.execute("SELECT * FROM employee_data WHERE NAME = ?", (name,))
+#         final_data = emp_cursor.fetchall()
+#         return final_data
 
 img = Image.open("/Users/arhan.sheth/Documents/Codes/DX/Flask/emp_details/dxFactor_logo.jpeg")
 
@@ -46,15 +46,24 @@ stl.title("Employee Database")
 # Prompting user to input employee name
 name = stl.text_input("Enter Employee Name", "Type Here...")
 if (stl.button("Submit")):
-    stl.warning("Generating URL... DO NOT CLOSE")
-    time.sleep(5)
     checker = emp_cursor.execute("SELECT EXISTS(SELECT 1 FROM employee_data WHERE NAME = ?)", (name,))
-    if name != 'Type Here...':
-        stl.success("Link Generated!")
-        flask_url = f'http://127.0.0.1:5000/getData/{name}'
-        time.sleep(2)
-        stl.info(f'Generated URL: {flask_url}')
-        app.run(port = 5000)
+    if checker == False:
+        stl.write("Employee Not Found!")
+    else:   
+        if name != 'Type Here...':
+            stl.success("Found Employee!")
+            time.sleep(1)
+            emp_cursor.execute("SELECT * FROM employee_data WHERE NAME = ?", (name,))
+            final_data = emp_cursor.fetchall()
+            for row in final_data:
+                f_name = row[0]
+                f_post = row[1]
+                f_loc = row[2]
+            stl.info(f"Employee Name: {f_name}")
+            stl.info(f"Employee Post: {f_post}") 
+            stl.info(f"Post Location: {f_loc}")
+        else:
+            stl.error("Please remove 'Type Here...' before writing employee name")
 
 emp_cursor.close()
 emp_connection.close()
